dfed8ee41e6251c8b62d7ae2198add1285187425
ChangHa001 <dohauzi@gmail.com>
2019-03-06 20:58:50 +0900

    conclustion

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
+  // 블록 해쉬 계산
   static calculateBlockHash = (
     index: number,
     previousHash: string,
@@ -9,6 +10,7 @@ class Block {
   ): string =>
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
+  // 블록 구조 유효성 체크
   static validateStructure = (aBlock: Block): boolean =>
     typeof aBlock.index === "number" &&
     typeof aBlock.hash === "string" &&
@@ -22,6 +24,7 @@ class Block {
   public data: string;
   public timestamp: number;
 
+  // Block 생성자
   constructor(
     index: number,
     hash: string,
@@ -37,16 +40,17 @@ class Block {
   }
 }
 
-const genesisBlock: Block = new Block(0, "2020202020", "", 123456, "Hello");
+const genesisBlock: Block = new Block(0, "2020202020", "", 123456, "Hello"); // 첫번째 블록
 
-let blockchain: Block[] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock]; // 블록체인
 
-const getBlockchain = (): Block[] => blockchain;
+const getBlockchain = (): Block[] => blockchain; // 블록체인 얻기
 
-const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1]; // 마지막 블록 얻기
 
-const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000); // 새 timestamp 얻기
 
+// 새로운 블록 생성
 const createNewBlock = (data: string): Block => {
   const previousBlock: Block = getLatestBlock();
   const newIndex: number = previousBlock.index + 1;
@@ -57,6 +61,8 @@ const createNewBlock = (data: string): Block => {
     newTimestamp,
     data
   );
+
+  // 새로운 블록
   const newBlock: Block = new Block(
     newIndex,
     newHash,
@@ -65,10 +71,21 @@ const createNewBlock = (data: string): Block => {
     data
   );
 
-  blockchain.push(newBlock);
+  addBlock(newBlock);
+
   return newBlock;
 };
 
+// 블록 hash값 계산
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+// 블록 유효성 검사
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
   if (!Block.validateStructure(candidateBlock)) {
     return false;
@@ -76,9 +93,25 @@ const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
     return false;
   } else if (previousBlock.hash !== candidateBlock.previousHash) {
     return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
   }
 };
 
-console.log(createNewBlock("Hello"), createNewBlock("bye bye"));
+// 블록체인에 블록 추가
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+
+createNewBlock("secondBlock");
+createNewBlock("thirdBlock");
+createNewBlock("fourthBlock");
+
+console.log(blockchain);
 
 export {};
